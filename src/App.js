331f4b6e8043d79334fb65f98eb8d32e52ade1f4
amar-at-iitm <ma24m002@smail.iitm.ac.in>
2025-10-25 20:37:01 +0530

    Tailwind setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <button className="px-6 py-3 bg-blue-600 text-white rounded-xl hover:bg-blue-700">
+        Tailwind is Working 
+      </button>
+    </div>
+  );
+}
