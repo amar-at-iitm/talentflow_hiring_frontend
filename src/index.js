094e6eeaf17af80b805438878535d9e5ef9780f4
amar-at-iitm <ma24m002@smail.iitm.ac.in>
2025-10-26 01:17:11 +0530

    Added MirageJS mock API with seeded jobs and basic routes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import { makeServer } from "./api/server";
+
+if (process.env.NODE_ENV === "development") {
+  makeServer();
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
